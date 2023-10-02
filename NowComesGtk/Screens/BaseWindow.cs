179a950622bbbd38be6b5c8aee37dff375045d13
Elijah <melissagouveia1306@gmail.com>
2023-10-02 15:33:02 -0300

    Organizaçõo/ Separação em ramos das linhas de código --- Novas telas e funcionalidades.

---
+++ NowComesGtk/Screens/BaseWindow.cs
@@ -0,0 +1,16 @@
+﻿using Gtk;
+
+namespace NowComesGtk.Screens
+{
+    public class BaseWindow : Window
+    {
+        public BaseWindow(string title, int width, int height) : base(WindowType.Toplevel)
+        {
+            Title = title;
+            Resizable = false;
+            SetPosition(WindowPosition.Center);
+            SetDefaultSize(width, height);
+            SetIconFromFile("Images/poketrainer_icon.png");
+        }
+    }
+}
---
+++ NowComesGtk/Utils/ButtonGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Gtk;
+
+namespace NowComesGtk.Reusable_components
+{
+    public class ButtonGenerator : Button
+    {
+        public ButtonGenerator(string imagePath, int width, int height) : base()
+        {
+            Image imgPokeball = new Image(imagePath);
+            SetSizeRequest(width, height);
+            Relief = ReliefStyle.None;
+            Image = imgPokeball;
+        }
+    }
+}
