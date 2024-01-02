96b3327533c709b9444293de812e13436a7f55ac
Kauã Freitas <83998740+Kaaunty@users.noreply.github.com>
2024-01-02 14:15:31 -0300

    add PokeTrainerAPI to build .net

---
+++ PokeTrainerAPI/Helper/PopulateLists.cs
@@ -0,0 +1,89 @@
+﻿using PokeTrainerBackEndTest.Entities;
+
+namespace PokeTrainerBackEnd.Helper
+{
+    public class PopulateLists
+    {
+        public List<Pokemon> GetPokemonListAll(int currentpage)
+        {
+            try
+            {
+                var pokemonList = Repository.Pokemon;
+                pokemonList = pokemonList.Skip(currentpage).Take(25).ToList();
+                return pokemonList;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Pokemon> GetPokemonListByTypeAll(int currentpage, string type)
+        {
+            string lowercasetype = type.ToLower();
+            try
+            {
+                var pokemonList = Repository.Pokemon;
+
+                Repository.pokemonListAllType = pokemonList.Where(pokemon => pokemon.Types.Any(type => type.Type.Name == lowercasetype)).ToList();
+                pokemonList = Repository.pokemonListAllType.Skip(currentpage).Take(25).ToList();
+                return pokemonList;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Pokemon> GetPokemonListByTypePure(int currentpage, string type)
+        {
+            string lowercasetype = type.ToLower();
+            try
+            {
+                var pokemonList = Repository.Pokemon;
+
+                Repository.pokemonListPureType = pokemonList.Where(pokemon => pokemon.Types.TrueForAll(type => type.Slot == 1) && pokemon.Types.Any(type => type.Type.Name == lowercasetype)).ToList();
+                pokemonList = Repository.pokemonListPureType.Skip(currentpage).Take(25).ToList();
+                return pokemonList;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Pokemon> GetPokemonListByTypeHalfType(int currentpage, string type)
+        {
+            string lowercasetype = type.ToLower();
+            try
+            {
+                var pokemonList = Repository.Pokemon;
+
+                Repository.pokemonListHalfType = pokemonList.Where(pokemon => pokemon.Types.Any(type => type.Type.Name == lowercasetype) && pokemon.Types.Any(type => type.Slot == 2)).ToList();
+                pokemonList = Repository.pokemonListHalfType.Skip(currentpage).Take(25).ToList();
+                return pokemonList;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Pokemon> GetPokemonlistByHalfTypeSecondary(int currentPage, string type)
+        {
+            string lowercasetype = type.ToLower();
+            try
+            {
+                var pokemonList = Repository.Pokemon;
+
+                Repository.pokemonListHalfSecundaryType = pokemonList.Where(pokemon => pokemon.Types.Count == 2 && pokemon.Types[1].Type.Name == lowercasetype).ToList();
+                pokemonList = Repository.pokemonListHalfSecundaryType.Skip(currentPage).Take(25).ToList();
+                return pokemonList;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ PokeTrainerAPI/Helper/XmlHelper.cs
@@ -0,0 +1,49 @@
+﻿using PokeTrainerBackEndTest.Entities;
+using PokeTrainerBackEndTest.Model;
+using System.Xml.Serialization;
+
+namespace PokeTrainerBackEnd.Helper
+{
+#nullable disable
+
+    public class XmlHelper
+    {
+        private PokemonModel pokemonModel = new PokemonModel();
+
+        public async Task XmlSerealizer()
+        {
+            try
+            {
+                List<Pokemon> pokemons = await pokemonModel.GetPokemonList();
+
+                XmlSerializer xmlSerealizer = new XmlSerializer(typeof(List<Pokemon>));
+                using (FileStream fs = new FileStream("Xml's/pokemonList.xml", FileMode.Create))
+                {
+                    xmlSerealizer.Serialize(fs, Repository.Pokemon);
+                    fs.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void XmlDeserealizerPokemonList()
+        {
+            try
+            {
+                XmlSerializer xmlSerealizer = new XmlSerializer(typeof(List<Pokemon>));
+                using (FileStream fs = new FileStream("Xml's/pokemonList.xml", FileMode.Open))
+                {
+                    Repository.pokemonList = (List<Pokemon>)xmlSerealizer.Deserialize(fs);
+                    fs.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
